9919d47d5ea6472db2a889d21c7006eef60c7b2d
irisnode <sd1602@srmist.edu.in>
2026-04-08 23:32:24 +0530

    UC2: Added Code of UC2

---
+++ HelloApp-UC-feature-UC2/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = args[0];
+        System.out.println( "Hello, " + name );
+    }
+}
